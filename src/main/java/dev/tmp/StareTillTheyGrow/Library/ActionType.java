31fdb8b4d55cfa19246e675ec0efc735d113a230
aeonic <ericknwash@gmail.com>
2022-05-31 14:08:26 -0500

    Delete ActionTypeHelper, move config checks to ActionType

--- src/main/java/dev/tmp/StareTillTheyGrow/Library/ActionType.java
+++ src/main/java/dev/tmp/StareTillTheyGrow/Library/ActionType.java
@@ -1,12 +1,24 @@
 package dev.tmp.StareTillTheyGrow.Library;
 
 import dev.tmp.StareTillTheyGrow.Config.Config;
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public enum ActionType {
-    FALL_IN_LOVE,
-    BONE_MEAL_BLOCK,
-    REGROW_WOOL,
-    GROW_UP,
-    CAKE_REGROWTH
-}
+    FALL_IN_LOVE(Config.COMMON.fallInLove),
+    BONE_MEAL_BLOCK(Config.COMMON.applyBoneMeal),
+    REGROW_WOOL(Config.COMMON.regrowWool),
+    GROW_UP(Config.COMMON.growBabies),
+    CAKE_REGROWTH(Config.COMMON.cakeRegrowth);
+
+    private final ForgeConfigSpec.BooleanValue configValue;
+
+    ActionType(ForgeConfigSpec.BooleanValue configValue) {
+        this.configValue = configValue;
+    }
+
+    public boolean enabled() {
+        return configValue.get();
+    }
+
+    }
 
