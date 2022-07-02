029b2ec3224b86055cd2bd54bf8beb1322e90141
Ian Wijma <ian@wij.ma>
2022-07-02 18:24:05 +1000

    Added todo's

--- src/main/java/dev/tmp/StareTillTheyGrow/StareTillTheyGrow.java
+++ src/main/java/dev/tmp/StareTillTheyGrow/StareTillTheyGrow.java
@@ -10,6 +10,23 @@ public class StareTillTheyGrow
     public static final String MOD_ID = "staretilltheygrow";
 
     public StareTillTheyGrow() {
-        // TODO: Make epic stuff
+        // TODO: [network] Cleaner communication between server and client
+
+        // TODO: [actions] More abstract handling of the server and client ticks
+
+        // TODO: [actions] Tick based action instead of time based
+
+        // TODO: [config] More organised config
+
+        // TODO: [config] Better whitelist / blacklist (block & entities)
+
+        // TODO: [config] White/blacklist support for mods
+
+        // TODO: [config] White/blacklist support for block groups
+
+        // TODO: [config] White/blacklist support for forge groups (crops for example)
+
+        // TODO: [config/actions] Configurable delays for each mod / group / action
+
     }
 }
